23d712b12b050f686a3e35aa01ba0a02f7a6960e
Lakshan Sithara <lakshansitharaabeysinghe@gmail.com>
2024-03-22 12:27:22 +0530

    created_all_parant_details_page

--- Schoolmanagementsystem/Addparentdetails.cs
+++ Schoolmanagementsystem/Addparentdetails.cs
@@ -49,12 +49,13 @@ namespace Schoolmanagementsystem
 
         private void button12_Click(object sender, EventArgs e)
         {
+            // Add parent details
             string connString = $"server={server};user={uid};database={database};password={password}";
             MySqlConnection conn = new MySqlConnection(connString);
             try
             {
                 conn.Open();
-                string insertQuery = "INSERT INTO parant (Name,NIC,DOB,Gender,Religion,Mobile_no,Address,Occupation,SID,relationship) VALUES('" + nameTB.Text + "','" + NICTB.Text + "','" + DOBDTB + "','" + gender + "','" + ReTB.Text + "','" + MobileTB + "','" + AddressTB.Text + "','"+OccTB.Text+"','"+CADTB+"','"+relationTB.Text +"') ";
+                string insertQuery = "INSERT INTO parant (Name,NIC,DOB,Gender,Religion,Mobile_no,Address,Occupation,SID,relationship,PID) VALUES('" + nameTB.Text + "','" + NICTB.Text + "','" + DOBDTB + "','" + gender + "','" + ReTB.Text + "','" + MobileTB + "','" + AddressTB.Text + "','" + OccTB.Text + "','" + CADTB + "','" + relationTB.Text + "','" + PIDTB + "') ";
                 MySqlCommand cmd = new MySqlCommand(insertQuery, conn);
                 int rowsAffected = cmd.ExecuteNonQuery();
                 if (rowsAffected > 0)
@@ -84,6 +85,107 @@ namespace Schoolmanagementsystem
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             gender = "female";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // Update parent details
+            string connString = $"server={server};user={uid};database={database};password={password}";
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+
+                // Assuming AID is a unique identifier in your academic table
+                string updateQuery = "UPDATE patant SET Name = @Name, NIC = @NIC, DOB = @DOB, Gender = @Gender, Religion = @Religion, " +
+                                     "Mobile_no = @MobileNo, Address = @Address,Occupation= @Occupation,SID=@SID,relationship=@relationship WHERE AID = @AID";
+
+                MySqlCommand cmd = new MySqlCommand(updateQuery, conn);
+                cmd.Parameters.AddWithValue("@Name", nameTB.Text);
+                cmd.Parameters.AddWithValue("@NIC", NICTB.Text);
+                cmd.Parameters.AddWithValue("@DOB", DOBDTB.Value.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Religion", ReTB.Text);
+                cmd.Parameters.AddWithValue("@MobileNo", MobileTB.Text);
+                cmd.Parameters.AddWithValue("@Address", AddressTB.Text);
+                cmd.Parameters.AddWithValue("@Occupation", OccTB);
+                cmd.Parameters.AddWithValue("@SID", CADTB.Text);
+                cmd.Parameters.AddWithValue("@relationship", relationTB.Text);
+                cmd.Parameters.AddWithValue("@PID", PIDTB.Text);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Parant details updated successfully");
+                }
+                else
+                {
+                    MessageBox.Show("No parant record with the specified Academic ID found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //delete parent record
+            string connString = $"server={server};user={uid};database={database};password={password}";
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+
+                string deleteQuery = "DELETE FROM parant WHERE PID = @PID";
+
+                MySqlCommand cmd = new MySqlCommand(deleteQuery, conn);
+                cmd.Parameters.AddWithValue("@AID", PIDTB.Text);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Parant record deleted successfully");
+                }
+                else
+                {
+                    MessageBox.Show("No Parant record with the specified Academic ID found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin();
+            admin.Show();
+            this.Hide();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin();
+            admin.Show();
+            this.Hide();
         }
     }
 }
