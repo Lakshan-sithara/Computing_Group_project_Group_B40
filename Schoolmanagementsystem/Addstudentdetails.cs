8ebcc5a38a6cbf641e21ae05410b884f2b9b5356
Lakshan Sithara <lakshansitharaabeysinghe@gmail.com>
2024-03-22 09:56:35 +0530

    remove_unnesasary_buttons

--- Schoolmanagementsystem/Addstudentdetails.cs
+++ Schoolmanagementsystem/Addstudentdetails.cs
@@ -174,9 +174,9 @@ namespace Schoolmanagementsystem
             try
             {
                 conn.Open();
-                string insertQuery = "INSERT INTO student (Name, Admission_number, DOB, Religion, Address, Father_name, Mother_name, gender,Grade,Class) " +
+                string insertQuery = "INSERT INTO student (Name, Admission_number, DOB, Religion, Address, Father_name, Mother_name, gender,Grade,Class,Addmission_datw) " +
                                      "VALUES ('" + nameTB.Text + "', '" + AdmiNuTB.Text + "', '" + DOBDTP.Value.ToString("yyyy-MM-dd") + "', '" + religionTB.Text + "', " +
-                                     "'" + addressTB.Text + "', '" + fatherTB.Text + "', '" + motherTB.Text + "', '" + Gender + "','" + grade + "','" + Class + ")";
+                                     "'" + addressTB.Text + "', '" + fatherTB.Text + "', '" + motherTB.Text + "', '" + Gender + "','" + grade + "','" + Class + ",'"+addmission_date_DTP.Value.ToString("yyyy-MM-dd")+"')";
                 MySqlCommand command = new MySqlCommand(insertQuery, conn);
 
                 int rowsAffected = command.ExecuteNonQuery();
